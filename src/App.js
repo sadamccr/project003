54f09597ee4323a4a037b0bbe43708d1d8e0c6dc
sadamccr <47504498+sadamccr@users.noreply.github.com>
2020-06-10 12:49:11 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br />
           Updated by Sadam.
         </p>
         <a
